358beabb124cc3d725ca96201286c0b3c654d70e
leo-mandala <leo.mandala@outlook.fr>
2023-11-16 16:19:05 +0100

    [+] adding style

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <div ref="search" class="search">
-      <input type="text" placeholder="your city" spellcheck="false" v-model="cityName" />
+      <input type="text" placeholder="Votre ville" spellcheck="false" v-model="cityName" />
       <button @click="searchWeather"><img class="icon" src="./icons/tuvois.jpg" alt="search" /></button>
     </div>
     <div ref="w-icon" class="weather">
@@ -10,14 +10,14 @@
       <h2 ref="cityNameResult" class="city">New York</h2>
       <div class="details">
         <div class="col">
-          <img src="./icons/humidity.jpg" class="subi" />
+          <img src="src/assets/droplet.png" class="cond-icon" />
           <div>
             <p ref="humy" class="humidity">%</p>
             <p>Humidity</p>
           </div>
         </div>
         <div class="col">
-          <img src="./icons/wind.jpg" class="subi" />
+          <img src="src/assets/wind.png" class="cond-icon" />
           <div>
             <p ref="nreaker" class="wind">50Km/H</p>
             <p>wind</p>
@@ -36,31 +36,43 @@ export default {
   data() {
     return {
       cityName: '',
-      weatherIcon: "./src/components/icons/claude.jpg", // default icon
+      weatherIcon: "./src/components/icons/clouds.png",
     };
   },
   mounted() {
     this.checkWeather();
   },
   methods: {
-    async checkWeather(city = 'Roubaix') {
-      const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
-      const data = await response.json();
-      console.log(data);
-      this.$refs.cityNameResult.innerHTML = data.name;
-      this.$refs.temps.innerHTML = Math.round(data.main.temp) + "°c";
-      this.$refs.humy.innerHTML = data.main.humidity + "%";
-      this.$refs.nreaker.innerHTML = data.wind.speed + "km/h";
-      if(data.weather[0].main === "Clouds"){
-        this.weatherIcon = "./src/components/icons/claude.jpg";
-      } else {
-        this.weatherIcon = "./src/components/sun.jpg";
-      }
-    },
-    searchWeather() {
-      this.checkWeather(this.cityName);
-    },
+  async checkWeather(city = 'Roubaix') {
+    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+    const data = await response.json();
+    console.log(data);
+    this.$refs.cityNameResult.innerHTML = data.name;
+    this.$refs.temps.innerHTML = Math.round(data.main.temp) + "°c";
+    this.$refs.humy.innerHTML = data.main.humidity + "%";
+    this.$refs.nreaker.innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main === "Clouds") {
+      this.weatherIcon = "./src/assets/clouds.png";
+    } else if (data.weather[0].main === "Clear") {
+      this.weatherIcon = "./src/assets/sun.png";
+    } else if (data.weather[0].main === "Rain") {
+      this.weatherIcon = "./src/assets/rain.png";
+    } else if (data.weather[0].main === "Snow") {
+      this.weatherIcon = "./src/assets/snow.png";
+    } else if (data.weather[0].main === "Thunderstorm") {
+      this.weatherIcon = "./src/assets/thunderstorm.png";
+    } else if (data.weather[0].main === "Drizzle") {
+      this.weatherIcon = "./src/assets/drizzle.png";
+    } else {
+      this.weatherIcon = "./src/assets/default.png";
+    }
   },
+  searchWeather() {
+    this.checkWeather(this.cityName);
+  },
+},
+
 };
 </script>
 
@@ -70,26 +82,28 @@ export default {
   padding: 40px 35px;
   border-radius: 30px;
 }
+
 .icon {
   width: 40px;
 }
+
 .search {
   display: flex;
   align-items: center;
 }
+
 .search input {
   border-radius: 30px;
 }
+
 .weather-icon {
-  width: 170px;
+  width: 40px;
 }
+
 .weather h1 {
   font-size: 80px;
 }
-.weather h1 {
-  font-size: 45px;
-  margin-top: -10px;
-}
+
 .details {
   display: flex;
   align-items: center;
@@ -97,7 +111,17 @@ export default {
   padding: 0 20px;
   margin-top: 50px;
 }
-.subi {
+
+.cond-icon {
   width: 40px;
+}
+
+@media only screen and (min-width: 768px) {
+
+  .card {
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto; 
+  }
 }
 </style>
