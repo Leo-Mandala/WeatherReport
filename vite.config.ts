9b619b502c53497f68ba2118ee8e7bf56d02b6da
leo-mandala <leo.mandala@outlook.fr>
2023-11-16 16:44:24 +0100

    [+] adding style

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      external: ['src/assets/droplet.png', 'src/assets/wind.png'],
+      
+    },
+  },
 })
